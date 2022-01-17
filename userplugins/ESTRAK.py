e7cdc526e3f1116f25f1ed2696102b49dc4bb8de
MAMO246 <90887327+MAMO246@users.noreply.github.com>
2022-01-17 19:55:08 +0300

    Create ESTRAK.py

---
+++ userplugins/ESTRAK.py
@@ -0,0 +1,25 @@
+import requests,telebot
+
+token = 'توكنك'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.chat.type == 'private':
+        ch = 'MAMO246@'
+        idd = message.chat.id
+        join = requests.get(f"https://api.telegram.org/bot{token}/getChatMember?chat_id=@{ch}&user_id={idd}").text
+        if '"status":"left"' in join:
+            bot.send_message(message.chat.id, f'''
+🚸| عذرا عزيزي
+🔰| عليك الاشتراك بقناة البوت لتتمكن من استخدامه
+
+- @{ch}
+
+‼️| اشترك ثم ارسل /start
+'''
+        else:
+            bot.send_message(message.chat.id,"اهلا")
+
+bot.polling()
