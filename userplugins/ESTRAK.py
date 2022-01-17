3ac38890071331d2d1489e947f83698aa2d422b8
MAMO246 <90887327+MAMO246@users.noreply.github.com>
2022-01-17 20:03:18 +0300

    Update ESTRAK.py

--- userplugins/ESTRAK.py
+++ userplugins/ESTRAK.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.chat.type == 'private':
-        ch = 'MAMO246@'
+        ch = 'MAMO246'
         idd = message.chat.id
         join = requests.get(f"https://api.telegram.org/bot{token}/getChatMember?chat_id=@{ch}&user_id={idd}").text
         if '"status":"left"' in join:
